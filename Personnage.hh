5271f3a7614b1312ab6c199158fa0aa69aed832e
touil <soumaya-touil@hotmail.fr>
2019-01-04 11:05:47 +0100

    Personnage.hh

--- Personnage.hh
+++ Personnage.hh
@@ -6,6 +6,11 @@ class Personnage{
 	Personnage(std :: string name )
 	~Personnage(){};
 	void choisir_personnage(int &a); 
+	void add_energy(int s){ _score += s;}
+	int energy()const{return _energy;}
+
+
+
  private:
-	int _score;
+	int _energy;
 }
