3c75cd564444083ccf2ea095a6942b817e108552
touil <soumaya-touil@hotmail.fr>
2019-01-21 11:37:55 +0100

    FIN

--- Question.h
+++ Question.h
@@ -12,8 +12,8 @@ public:
 	 ~Question(){};
 	 int get_num_question(){return _num_question;} // numéro de la question posée
 	 int set_num_question();
-     void operator()(int  val);
-	 void init();
+     void operator()(int  val);//Cette fonction permet de verifier quel personnage est choisi, et en fonction du personnage choisi ,on affiche les questions de ce dernier à partir du fichier .txt correspodant . 
+	 void init();// Tant que le fichier est parcouru , on ajoute à _liste les liens vers les images
 
 
 
