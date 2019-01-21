0269d11a054599d44895611fe93ffbf3b60da99a
touil <soumaya-touil@hotmail.fr>
2019-01-21 11:31:59 +0100

    Version commenté

--- Deplacement.cc
+++ Deplacement.cc
@@ -1,7 +1,9 @@
 #include "Deplacement.hh"
 
+// Le personnage est placé dans un coin précis de la fenêtre 
  void Deplacement::init(Personnage &p)
 {
+	//on prend ici le coin du bas sur l'image d'acceuil 
 	p.set_PosPerso_x(50);
 	p.set_PosPerso_y(400);
 	p.set_PosPerso_w(25);
@@ -11,7 +13,7 @@
 
 };
 
-
+//cette fonction permet de deplacer le personnage , le personnage se déplace à droite si sa réponse est bonne, à gauche sinon
 void Deplacement::deplace(Personnage &p, bool b)
 {
 	SDL_Rect  tmp=p.get_PosPerso();
